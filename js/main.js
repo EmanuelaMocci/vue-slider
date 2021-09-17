a5fa74ad8e38140d94a3d28afd5576e9840574b2
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-17 16:14:47 +0200

    Aggiunta immagini

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ var app = new Vue({
     el: '#root',
     data: {
         titolo: "Visita le nostre cale",
-        image: 'mare-1.jpeg',
-        logo: 'logo.png'
+        image: [ "img/mare-1.jpeg", "img/mare-2.jpeg", "img/mare-3.jpeg" ],
+        logo: 'img/logo.png',
+        imageIndex: 0
+    },
+    methods: {
+
     }
 })
